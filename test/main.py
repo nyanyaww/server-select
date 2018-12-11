7efa842bd73246e89ba86714e30d705d90ebb199
李聪盛 <515478826@qq.com>
2018-12-12 03:14:52 +0800

    none

--- test/main.py
+++ test/main.py
@@ -12,15 +12,6 @@ import os
 import time
 import random
 
-
-def long_time_task(name):
-    print('Run task %s (%s)...' % (name, os.getpid()))
-    start = time.time()
-    time.sleep(random.random() * 3)
-    end = time.time()
-    print('Task %s runs %0.2f seconds.' % (name, (end - start)))
-
-
 if __name__ == '__main__':
     print('Parent process %s.' % os.getpid())
     p = Pool(3)
